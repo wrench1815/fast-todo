74106b56c7aa866f660f6cb2c765fa53e7a52881
wrench1815 <hardeepkumar1815@gmail.com>
2022-11-22 23:52:37 +0530

    📦 NEW: add filters to show completed, pending or all tasks

--- front/src/views/HomeView.vue
+++ front/src/views/HomeView.vue
@@ -6,6 +6,7 @@ import AddTask from '@/components/AddTask.vue';
 
 const tasks = ref([]);
 const loading = ref(true);
+const filter = ref('all');
 
 const axios = inject('axios') as AxiosStatic;
 
@@ -22,6 +23,23 @@ function refreshTasks() {
     loading.value = false;
   });
 }
+
+function filterTasks() {
+  if (filter.value == 'completed') {
+    return axios.get('todo?completed=true').then((r) => {
+      tasks.value = r.data;
+    });
+  } else if (filter.value == 'pending') {
+    return axios.get('todo?pending=true').then((r) => {
+      tasks.value = r.data;
+    });
+  } else {
+    return axios.get('todo/').then((r) => {
+      tasks.value = r.data;
+    });
+  }
+}
+
 onMounted(() => {
   getTasks().then(() => {
     loading.value = false;
@@ -31,6 +49,55 @@ onMounted(() => {
 
 <template>
   <main class="container">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Filters</h5>
+
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterRadios"
+            id="all"
+            value="all"
+            v-model="filter"
+          />
+          <label class="form-check-label" for="all"> All </label>
+        </div>
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterRadios"
+            id="completed"
+            value="completed"
+            v-model="filter"
+          />
+          <label class="form-check-label" for="completed"> Completed </label>
+        </div>
+
+        <div class="form-check">
+          <input
+            class="form-check-input"
+            type="radio"
+            name="filterRadios"
+            id="pending"
+            value="pending"
+            v-model="filter"
+          />
+          <label class="form-check-label" for="pending"> Pending </label>
+        </div>
+
+        <button
+          type="button"
+          class="btn btn-primary btn-sm btn-rounded mt-3"
+          @click="filterTasks"
+        >
+          submit
+        </button>
+      </div>
+    </div>
+
     <div class="d-flex justify-content-end my-4">
       <button
         class="btn btn-primary btn-rounded"
