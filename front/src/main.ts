d9f0a82a4db5c3d1bfaa597488b99641344e858a
wrench1815 <hardeepkumar1815@gmail.com>
2022-11-22 22:18:22 +0530

    👌 IMPROVE: add axios

--- front/src/main.ts
+++ front/src/main.ts
@@ -2,9 +2,8 @@ import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
 
-import './assets/main.css';
-
-import axios from './plugins/axios';
+import axios from 'axios';
+import VueAxios from 'vue-axios';
 
 import * as mdb from 'mdb-ui-kit';
 import 'mdb-ui-kit/css/mdb.min.css';
@@ -12,8 +11,9 @@ import 'mdb-ui-kit/css/mdb.min.css';
 const app = createApp(App);
 
 app.use(router, mdb);
-app.use(axios, {
-  baseUrl: 'http://localhost:8000',
-});
+
+app.use(VueAxios, axios);
+app.provide('axios', app.config.globalProperties.axios);
+axios.defaults.baseURL = 'http://localhost:8000/';
 
 app.mount('#app');
